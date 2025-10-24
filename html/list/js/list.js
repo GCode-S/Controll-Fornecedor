d911d9c0f401a8e0a5282c4544b5e601822c9364
Gabriel Souza <63251476+GCode-S@users.noreply.github.com>
2025-10-24 08:51:12 -0300

    feat:add script AdSense

--- html/list/js/list.js
+++ html/list/js/list.js
@@ -194,7 +194,17 @@ window.viewProduct = async function (){
                         </div>
    
                 </div>
-                
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6913696668328445"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-fb+5w+4e-db+86"
+     data-ad-client="ca-pub-6913696668328445"
+     data-ad-slot="8693383431"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
                `;
 
               
